e3e5f75ebf32c9cf24e5bb1655e8e15b0dbe4e4d
Nicolas Torre <nicolas.torre@innew.la>
2022-03-19 19:51:53 -0300

    Refactor input vacio

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 function calcular() {
-    let nombre = document.getElementById("nombre").value;
-    let peso = document.getElementById("peso").value;
-    let altura = document.getElementById("altura").value;
-    let metros = altura / 100;
-    let cuadradoAltura = Math.pow(metros, 2)
-    let resultado = peso / cuadradoAltura;
-    let final = resultado.toFixed(2);
-    let resultContent = document.getElementById("result");
-    document.getElementById("masa").innerHTML = `IMC: ${final}`;
-    if (resultado > 25) {
-        document.getElementById("frase").innerHTML = `${nombre} estas gordito.`;
-        resultContent.classList.add('bad');
-        document.body.classList.add('fat')
-    } else {
-        document.getElementById("frase").innerHTML = `${nombre} estas bien de peso.`;
-        resultContent.classList.add('fine');
-        document.body.classList.add('slim')
+    if (document.getElementById("peso").value.length > 0, document.getElementById("altura").value.length > 0) {
+        let nombre = document.getElementById("nombre").value;
+        let peso = document.getElementById("peso").value;
+        let altura = document.getElementById("altura").value;
+        let metros = altura / 100;
+        let cuadradoAltura = Math.pow(metros, 2)
+        let resultado = peso / cuadradoAltura;
+        let final = resultado.toFixed(2);
+        let resultContent = document.getElementById("result");
+        document.getElementById("masa").innerHTML = `IMC: ${final}`;
+        if (resultado > 25) {
+            document.getElementById("frase").innerHTML = `${nombre} estas gordito.`;
+            resultContent.classList.add('bad');
+            document.body.classList.add('fat')
+        } else {
+            document.getElementById("frase").innerHTML = `${nombre} estas bien de peso.`;
+            resultContent.classList.add('fine');
+            document.body.classList.add('slim')
+        }
+    }
+    else {
+        document.getElementById("frase").innerHTML = `<p class="alert">Faltan uno o más datos.</p>`;
     }
 }
 
@@ -27,4 +32,4 @@ document.getElementById("mic").addEventListener("keyup", function (event) {
     if (event.key == "Enter") {
         calcular()
     }
-  }); 
+}); 
